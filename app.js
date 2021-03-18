9ea9f6756350c7044abf9e80574a3eae479b2a43
NielsPeeters96 <78353674+NielsPeeters96@users.noreply.github.com>
2021-03-18 12:07:07 +0100

    Node JS

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Using static files from static directory
+app.use(express.static('public'));
+app.use('/js', express.static(__dirname + 'public/js'));
+app.use('/img', express.static(__dirname + 'public/img'));
+app.use('/css', express.static(__dirname + 'public/css'));
+
+// Setting views (EJS)
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.get('/', async (req, res) => {
+      res.render('home.ejs');
+    })
+
+app.get('/bestelpagina', function (req, res) {
+    res.render('bestelpagina.ejs');
+  });
+
+app.get("/overzicht", (req, res) => {
+  res.render("overzichtspagina.ejs");
+});
+
+app.listen(port, () => console.log(`App is running on port ${port}`));
