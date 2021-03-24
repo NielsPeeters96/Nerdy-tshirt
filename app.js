834cf30aa4a4005836329c0b5244b394cdc3343f
NielsPeeters96 <78353674+NielsPeeters96@users.noreply.github.com>
2021-03-24 16:51:26 +0100

    port

--- app.js
+++ app.js
@@ -42,4 +42,4 @@ app.post("/winkelmand", urlencodedParser, (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => console.log("Listening to port " + PORT));
+app.listen(port, () => console.log("Listening to port " + port));
