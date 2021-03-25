18a2dc7500b31489a746a61c46f634f39aff6070
Niels Peeters <niels.peeters@hva.nl>
2021-03-25 12:32:46 +0100

    fixed js

--- Public/js/main.js
+++ Public/js/main.js
@@ -1,26 +1,17 @@
-function fillShirt() {
-    var white = document.getElementById("color-white");
-    var black = document.getElementById("color-black");
-    var blue = document.getElementById("color-blue");
-    var yellow = document.getElementById("color-yellow");
-    if (white) {
-      white.addEventListener("change", () => {
-        changeColor(white.value);
-      });
-      black.addEventListener("change", () => {
-        changeColor(black.value);
-      });
-      blue.addEventListener("change", () => {
-        changeColor(blue.value);
-      });
-      yellow.addEventListener("change", () => {
-        changeColor(yellow.value);
-      });
-    }
-  }
-  
-  function changeColor(value) {
-    const shirt = document.getElementById("t-shirt");
+const fieldset = document.getElementById("kleur");
+const radiobuttons = fieldset.querySelectorAll('input[type=radio]');
+console.log(fieldset);
+console.log(radiobuttons);
+
+radiobuttons.forEach((radiobutton) =>{ 
+  radiobutton.addEventListener("change", (event) => {
+    changeColor(event.target.value);
+})
+})
+
+function changeColor(value) {
+    const shirt = document.getElementById("changeColor");
   
     shirt.style.fill = value;
-  }
+    console.log(value);
+}
