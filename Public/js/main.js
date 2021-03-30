d425b4ff5f6b32f8c23c9610c4f19190033fbc0a
NielsPeeters96 <78353674+NielsPeeters96@users.noreply.github.com>
2021-03-30 17:08:57 +0200

    Update

--- Public/js/main.js
+++ Public/js/main.js
@@ -75,48 +75,17 @@ radioGeslacht.forEach((radiobutton) => {
   }
 })
 }
-function formValidation() {
-  const form = document.getElementById("bestelForm");
-  const naamForm = document.querySelectorAll("naamForm");
-  const postcodeForm = document.querySelectorAll("postcodeForm");
-  const mobielForm = document.querySelectorAll('mobielForm')
-  const adresForm = document.querySelectorAll("adresForm");
-  const errorElement = document.getElementById("error");
 
-  naamForm.forEach((form) => {
-    form.required = false
-  })
-  postcodeForm.forEach((form) => {
-    form.required = false
-  })
-  mobielForm.forEach((form) => {
-    form.required = false
-  })
-  adresForm.forEach((form) => {
-      form.required = false
-  })
+function updateText(event) {
+  if ('URLSearchParams' in window) {
+    // Set shirt text
+    const shirtText = document.getElementById('shirt-text');
+    shirtText.innerHTML = event.value;
 
-  form.addEventListener("submit", (e) => {
-    let messages = [];
-    if (![...naamForm].some((el) => el.checked == true)) {
-      messages.push("Vul je naam in");
-    }
-    if (![...postcodeForm].some((el) => el.checked == true)) {
-      messages.push("Vul je postcode in");
-    }
-    if (![...mobielForm].some((el) => el.checked == true)) {
-      messages.push("Vul je nummer in");
-    }
-    if (![...adresForm].some((el) => el.checked == true)) {
-      messages.push("Vul je adres in");
-    }
-
-    if (messages.length > 0) {
-      e.preventDefault();
-      errorElement.innerText = messages.join(", ");
-    }
-  });
+    updateQueryValue('text', event.value)
+  }
 }
 
 storeColor();
+updateText();
 
--- Public/js/removeButton.js
+++
@@ -1,3 +0,0 @@
-export function removeButton() {
-    document.getElementById("buttonKleur").style.display='none';
-}
