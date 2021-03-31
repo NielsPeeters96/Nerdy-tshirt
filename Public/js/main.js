1217a8ad90e5e0f8c68a9db3d4139ca6e4099020
Niels Peeters <niels.peeters@hva.nl>
2021-03-31 13:16:21 +0200

    Merge branch 'master' of https://github.com/NielsPeeters96/Nerdy-tshirt

--- Public/js/main.js
+++ Public/js/main.js
@@ -1,44 +1,3 @@
-function formValidation() {
-  const form = document.getElementById("bestelForm");
-  const naamForm = document.querySelector("#naamForm input");
-  const postcodeForm = document.querySelector("#postcodeForm input");
-  const adresForm = document.querySelector("#adresForm input");
-  const mobielForm = document.querySelector("#mobielForm input");
-  const errorElement = document.getElementById("errorMsg");
-
-  // if javascript is enabled remove required attributes
-  
-    naamForm.required = false
-    postcodeForm.required = false
-    adresForm.required = false
-    mobielForm.required = false
-
-  // all inputs aren't checked, these messages gets pushed into an array
-  form.addEventListener("submit", (e) => {
-    let messages = []
-    if (naamForm.value.length <= 0) {
-      messages.push('Naam niet ingevuld')
-    }
-    if (postcodeForm.value.length <= 0) {
-      messages.push('Postcode niet ingevuld')
-    }
-    if (adresForm.value.length <= 0) {
-      messages.push('Adres niet ingevuld')
-    }
-    if (mobielForm.value.length <= 0) {
-      messages.push('Mobiel niet ingevuld')
-    }
-
-    // if this array is not empty prevent submit and set fill error element with error text
-    if (messages.length > 0) {
-      e.preventDefault();
-      errorElement.innerText = messages.join(", ");
-    }
-  });
-}
-
-formValidation();
-
 const fieldset = document.getElementById("kleur");
 const radioKleur = fieldset.querySelectorAll('input[type=radio]');
 const fieldsetMaat = document.getElementById("maat");
@@ -81,6 +40,8 @@ radioGeslacht.forEach((radiobutton) =>{
 })
 }
 
+storeColor();
+
 window.onload = () => getColor()
 
 function getColor(){
@@ -117,6 +78,57 @@ radioGeslacht.forEach((radiobutton) => {
 })
 }
 
-storeColor();
+function tekstTshirt() {
+  const textInput = document.getElementById("userText");
+  const editText = document.getElementById("tekstTshirt");
+
+  if (textInput) {
+    textInput.addEventListener("keyup", () => {
+      editText.innerText = textInput.value;
+    });
+  }
+}
+
 tekstTshirt();
+
+function formValidation() {
+  const form = document.getElementById("bestelForm");
+  const naamForm = document.querySelector("#naamForm input");
+  const postcodeForm = document.querySelector("#postcodeForm input");
+  const adresForm = document.querySelector("#adresForm input");
+  const mobielForm = document.querySelector("#mobielForm input");
+  const errorElement = document.getElementById("errorMsg");
+
+  // if javascript is enabled remove required attributes
+  
+    naamForm.required = false
+    postcodeForm.required = false
+    adresForm.required = false
+    mobielForm.required = false
+
+  // all inputs aren't checked, these messages gets pushed into an array
+  form.addEventListener("submit", (e) => {
+    let messages = []
+    if (naamForm.value.length <= 0) {
+      messages.push('Naam niet ingevuld')
+    }
+    if (postcodeForm.value.length <= 0) {
+      messages.push('Postcode niet ingevuld')
+    }
+    if (adresForm.value.length <= 0) {
+      messages.push('Adres niet ingevuld')
+    }
+    if (mobielForm.value.length <= 0) {
+      messages.push('Mobiel niet ingevuld')
+    }
+
+    // if this array is not empty prevent submit and set fill error element with error text
+    if (messages.length > 0) {
+      e.preventDefault();
+      errorElement.innerText = messages.join(", ");
+    }
+  });
+}
+
+formValidation();
 
